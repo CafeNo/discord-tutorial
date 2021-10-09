1f57dcb1d96bed54814826585c7d6e4e5811703c
CafeNo <78199114+CafeNo@users.noreply.github.com>
2021-10-09 21:15:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='-',help_command=None)
+bot = commands.Bot(command_prefix='-',help_command=None) 
 
+# doing when client has started
 @bot.event
 async def on_ready():
     print(f'{bot.user} has Logged')
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Couupe Ai"))
 
+# doing with profix
 @bot.command()
 async def help(ctx):
-    await ctx.channel.send('ไม่มีคำสั่ง ขี้เกียจทำ')
+    await ctx.channel.send('All variable commands')
 
 @bot.command()
 async def test1(ctx):
-    await ctx.channel.send('TEST เฉยๆไม่มีอะไร')
+    await ctx.channel.send('JUST TEST')
 
-
+# doing without prefix
 @bot.event
 async def on_message(message):
-    if message.content == '123':
+    if message.content == 'Hello':
         print(message.channel)
         print('HELLO FUNCTION')
         await message.channel.send('HELLO!')
     elif message.content == '789654123564':
         await bot.logout
     await bot.process_commands(message)
+
 
 bot.run(token)
