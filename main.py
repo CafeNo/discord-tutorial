e6d06854ba786cb7ef9ee5f7aca0e32df9c738d7
CafeNo <78199114+CafeNo@users.noreply.github.com>
2021-11-16 15:03:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='-',help_command=None) 
+bot = commands.Bot(command_prefix='-',help_command=None)  # prefix configation and disable default help command
 
 # doing when client has started
 @bot.event
 async def on_ready():
     print(f'{bot.user} has Logged')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Couupe Ai"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Couupe Ai")) #Display status
 
-# doing with profix
+# doing with prefix
 @bot.command()
-async def help(ctx):
+async def help(ctx): # help command used with prefix
     await ctx.channel.send('All variable commands')
 
 @bot.command()
@@ -20,14 +20,14 @@ async def test1(ctx):
 
 # doing without prefix
 @bot.event
-async def on_message(message):
+async def on_message(message): # message command doesn't used prefix
     if message.content == 'Hello':
         print(message.channel)
         print('HELLO FUNCTION')
         await message.channel.send('HELLO!')
     elif message.content == '789654123564':
         await bot.logout
-    await bot.process_commands(message)
+    await bot.process_commands(message) 
 
 
-bot.run(token)
+bot.run(token) # add your token
